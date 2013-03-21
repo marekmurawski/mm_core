7d16678d045fdc69f4d362d6cd5ec9ca932fdfc3
marekmurawski <marek.murawski@gmail.com>
2013-03-21 10:55:22 +0100

    documentation

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 <?php
 
+/*
+ * Wolf CMS - Content Management Simplified. <http://www.wolfcms.org>
+ * Copyright (C) 2008-2010 Martijn van der Kleijn <martijn.niji@gmail.com>
+ *
+ * MultiEdit Plugin for Wolf CMS
+ * Provides convenient interface to quickly edit multiple pages metadata.
+ *
+ * @package Plugins
+ * @subpackage multiedit
+ *
+ * @author Marek Murawski <http://marekmurawski.pl>
+ * @copyright Marek Murawski, 2012
+ * @license http://www.gnu.org/licenses/gpl.html GPLv3 license
+ */
+
 /* Security measure */
-if ( !defined('IN_CMS') )
+if ( !defined('IN_CMS') ) {
     exit();
+}
 
 Plugin::setInfos(array(
             'id'                   => 'mm_core',
@@ -12,6 +28,7 @@ Plugin::setInfos(array(
             'license'              => 'GPL',
             'author'               => 'Marek Murawski',
             'website'              => 'http://marekmurawski.pl/',
+            'update_url'           => 'http://marekmurawski.pl/static/wolfplugins/plugin-versions.xml',
             'require_wolf_version' => '0.7.3'
 ));
 
