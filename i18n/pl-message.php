23f6489f1699aa32d5225a4cad344218e05742eb
marekmurawski <marek.murawski@gmail.com>
2013-03-21 10:36:00 +0100

    pl translation

---
+++ i18n/pl-message.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Polish language file for plugin mm_core
+ *
+ * @package Plugins
+ * @subpackage mm_core
+ *
+ * @author Your Name <http://marekmurawski.pl>
+ * @version Wolf 0.7.5
+ */
+return array(
+            'Assigned permission <b>:perm</b> to role <b>:role</b>!'                                => 'Przypisano uprawnienie <b>:perm</b> do roli <b>:role</b>!',
+            'Could not assign permission <b>:perm</b> to role <b>:role</b>!'                        => 'Nie udało się przypisać uprawnienia <b>:perm</b> do roli <b>:role</b>!',
+            'Could not create role <b>:role</b>'                                                    => 'Nie udało się utworzyć roli <b>:role</b>'                                                   ,
+            'Created role <b>:role</b>'                                                             => 'Utworzono rolę <b>:role</b>'                                                            ,
+            'Either permission <b>:perm</b> or role <b>:role</b> does not exist!'                   => 'Uprawnienie <b>:perm</b> lub rola <b>:role</b> nie istnieje!'                  ,
+            'Imported <b>:count</b> snippets! <br/><b>:names</b>'                                   => 'Zaimportowano <b>:count</b> skrawków! <br/><b>:names</b>'                                  ,
+            'Permission <b>:perm</b> already exists'                                                => 'Uprawnienie <b>:perm</b> już istnieje'                                               ,
+            'Permission <b>:perm</b> could not be created'                                          => 'Uprawnienie <b>:perm</b> nie mogło zostać utworzone'                                         ,
+            'Permission <b>:perm</b> could not be deleted'                                          => 'Uprawnienie <b>:perm</b> nie mogło zostać usunięte'                                         ,
+            'Permission <b>:perm</b> created'                                                       => 'Uprawnienie <b>:perm</b> utworzone'                                                      ,
+            'Permission <b>:perm</b> deleted'                                                       => 'Uprawnienie <b>:perm</b> usunięte'                                                      ,
+            'Permission <b>:perm</b> was not found and not deleted!'                                => 'Uprawnienie <b>:perm</b> nie znalezione i nie zostało usunięte!'                               ,
+            'Role <b>:role</b> already exists!'                                                     => 'Rola <b>:role</b> już istnieje!'                                                    ,
+            'Role <b>:role</b> already has permission <b>:perm</b>!'                                => 'Rola <b>:role</b> już ma uprawnienie <b>:perm</b>!'                               ,
+            'Role <b>:role</b> could not be deleted'                                                => 'Rola <b>:role</b> nie mogła zostać usunięta'                                               ,
+            'Role <b>:role</b> deleted!'                                                            => 'Rola <b>:role</b> usunięta!'                                                           ,
+            'Role <b>:role</b> has some permissions - cannot delete role with existing permissions' => 'Rola <b>:role</b> posiada przypisane uprawnienia - nie można usunąć roli z uprawnieniami',
+            'Role <b>:role</b> was not found and not deleted!'                                      => 'Rola <b>:role</b> nie znaleziona i nie została usunięta!'                                     ,
+            'Successfully activated <b>mm_core</b> plugin'                                          => 'Pomyślnie aktywowano plugin <b>mm_core</b>'                                         ,
+            'Successfully activated plugin <b>:name</b>'                                            => 'Pomyślnie aktywowano plugin <b>:name</b>'                                           ,
+//            'Support plugin for mm plugins.'                                                        => 'Support plugin for mm plugins.'                                                       ,
+//            'mmCore library'                                                                        => 'mmCore library'                                                                       ,
+);
