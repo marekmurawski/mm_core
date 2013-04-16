dcd4c56ef1cb209e2f463a8fafac73d7b6e995e7
marekmurawski <marek.murawski@gmail.com>
2013-04-16 08:20:16 +0200

    i18n

---
+++ i18n/en-message.php
@@ -0,0 +1,36 @@
+<?php
+
+    /**
+     * EN file for plugin mm_core
+     *
+     * @package Plugins
+     * @subpackage mm_core
+     *
+     * @author Your Name <email@domain.something>
+     * @version Wolf x.y.z
+     */
+
+    return array(
+    	'Assigned permission <b>:perm</b> to role <b>:role</b>!' => 'Assigned permission <b>:perm</b> to role <b>:role</b>!',
+	'Could not assign permission <b>:perm</b> to role <b>:role</b>!' => 'Could not assign permission <b>:perm</b> to role <b>:role</b>!',
+	'Could not create role <b>:role</b>' => 'Could not create role <b>:role</b>',
+	'Created role <b>:role</b>' => 'Created role <b>:role</b>',
+	'Either permission <b>:perm</b> or role <b>:role</b> does not exist!' => 'Either permission <b>:perm</b> or role <b>:role</b> does not exist!',
+	'Imported <b>:count</b> snippets! <br/><b>:names</b>' => 'Imported <b>:count</b> snippets! <br/><b>:names</b>',
+	'Permission <b>:perm</b> already exists' => 'Permission <b>:perm</b> already exists',
+	'Permission <b>:perm</b> could not be created' => 'Permission <b>:perm</b> could not be created',
+	'Permission <b>:perm</b> could not be deleted' => 'Permission <b>:perm</b> could not be deleted',
+	'Permission <b>:perm</b> created' => 'Permission <b>:perm</b> created',
+	'Permission <b>:perm</b> deleted' => 'Permission <b>:perm</b> deleted',
+	'Permission <b>:perm</b> was not found and not deleted!' => 'Permission <b>:perm</b> was not found and not deleted!',
+	'Role <b>:role</b> already exists!' => 'Role <b>:role</b> already exists!',
+	'Role <b>:role</b> already has permission <b>:perm</b>!' => 'Role <b>:role</b> already has permission <b>:perm</b>!',
+	'Role <b>:role</b> could not be deleted' => 'Role <b>:role</b> could not be deleted',
+	'Role <b>:role</b> deleted!' => 'Role <b>:role</b> deleted!',
+	'Role <b>:role</b> has some permissions - cannot delete role with existing permissions' => 'Role <b>:role</b> has some permissions - cannot delete role with existing permissions',
+	'Role <b>:role</b> was not found and not deleted!' => 'Role <b>:role</b> was not found and not deleted!',
+	'Successfully activated <b>mm_core</b> plugin' => 'Successfully activated <b>mm_core</b> plugin',
+	'Successfully activated plugin <b>:name</b>' => 'Successfully activated plugin <b>:name</b>',
+	'Support plugin for mm plugins.' => 'Support plugin for mm plugins.',
+	'mmCore library' => 'mmCore library',
+    );
